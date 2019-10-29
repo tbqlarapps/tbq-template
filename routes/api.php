f993fc9296999c25db6c370cdba5498b29ff6631
尘土918 <tbq2616@sina.com>
2019-10-29 11:43:04 +0800

    my zhanshen

--- routes/api.php
+++ routes/api.php
@@ -16,19 +16,18 @@ Route::group(['prefix' => 'v1'], function () {
     Route::get('foo', function () {
         return 'Hello World';
     });
-});
-
-Route::namespace ('\App\Api\Controllers')->group(function () {
-    /***
-     * 管理员后台接口路由
-     */
-    Route::prefix('user')->group(function () {
-        Route::post('register', 'UserController@register');
-        Route::post('login', 'UserController@login');
+    Route::namespace ('\App\Api\Controllers')->group(function () {
+        /***
+         * 管理员后台接口路由
+         */
+        Route::prefix('user')->group(function () {
+            Route::post('register', 'UserController@register');
+            Route::post('login', 'UserController@login');
+        });
+        // Route::prefix('admin')->namespace('Admin')->middleware('auth:admins')->group(function () {
+        //     Route::get('/', 'HomeController@index'); //后台首页
+        // });
     });
-    // Route::prefix('admin')->namespace('Admin')->middleware('auth:admins')->group(function () {
-    //     Route::get('/', 'HomeController@index'); //后台首页
-    // });
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
